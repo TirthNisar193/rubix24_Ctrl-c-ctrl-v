c49bf028eeafc318f68d68c9a84c050ff491a70f
GBhosle <gaurib49@gmail.com>
2024-01-18 01:20:24 +0530

    Changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import './App.css'
+// import ResourceAllocationMap from './components/ResourceAllocationMap'
 import SeismicActivityGraphs from './components/SeismicActivityGraphs'
+// import EmergencyDeploymentMap from './components/EmergencyDeploymentMap'
+import Home from './layout/Home'
 
 function App() {
 
   return (
-    <>
-      <SeismicActivityGraphs />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/seismic' element={<SeismicActivityGraphs/>}/>
+        {/* <Route path='/map' element={<ResourceAllocationMap />}/>
+        <Route path='/EmergencyDeploymentMap' element={<EmergencyDeploymentMap/>} /> */}
+      </Routes>  
+    </BrowserRouter>
   )
 }
 
