2620a841e9fb4d5dc14af26e5fbccb94042c2299
MeetMRP <meetpatelmrp1005@gmail.com>
2024-01-18 15:00:00 +0530

    minor

--- backend/main.py
+++ backend/main.py
@@ -28,14 +28,28 @@ async def retrieve_news(topic: str):
 
 
 @app.get("/twitter")
-async def retrieve_twitter_data(word: str, number: int = 5):
+async def retrieve_twitter_data(word: str, number: int = 7):
     if not word:
         raise HTTPException(status_code=400, detail="No word provided")
-    return get_tweets(word=word, number=number) #connect to db for better speed
-
+    client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
+    database = client[MONGO_DATABASE]
+    collection = database['twitter']
+    
+    try:
+        data_in_db = await collection.find_one({'word': word}, {'_id': 0})
+        if not data_in_db:
+            data = get_tweets(word=word, number=number)
+            await collection.insert_one(data)
+            return data
+        else:
+            return data_in_db
+    except Exception as e:
+        raise Exception('some error occured: ',e)
 
 @app.post("/add_docter")
 async def add_docter(items: List[Dict]):
+    if not items:
+        raise HTTPException(status_code=400, detail="No items provided")
     client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
     database = client[MONGO_DATABASE]
     collection = database['docter']
@@ -53,8 +67,11 @@ async def add_docter(items: List[Dict]):
 
     return {"status": True}
 
+
 @app.get("/get_docters")
 async def get_docter(location: str, count: int = 5):
+    if not location:
+        raise HTTPException(status_code=400, detail="No location provided")
     client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
     database = client[MONGO_DATABASE]
     collection = database['docter']
@@ -73,6 +90,7 @@ async def get_docter(location: str, count: int = 5):
     finally:
         client.close()
 
+
 @app.get("/live_stats")
 async def get_live_stats(tag_name: str = 'naturaldisaster'):
     client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
@@ -80,7 +98,7 @@ async def get_live_stats(tag_name: str = 'naturaldisaster'):
     collection = database['social']
 
     try:
-        data_in_db = await collection.find_one({'tag_name': tag_name})
+        data_in_db = await collection.find_one({'tag_name': tag_name}, {'_id': 0})
         if not data_in_db:
             data = scrape_social_buzz(tag_name=tag_name)
             await collection.insert_one(data)
